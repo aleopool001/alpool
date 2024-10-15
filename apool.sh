860b701a6f1b01718a341c4b5440fea50ece7b65
aleopool001 <aleopool001@outlook.com>
2024-10-15 18:10:06 +0530

    Update apool.sh

--- apool.sh
+++ apool.sh
@@ -1,3 +1,3 @@
 wget https://github.com/aleopool001/alpool/releases/download/0.001/apoolminer
 chmod +x apoolminer
-./apoolminer --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_7cqdjrvl2n --worker worker_01 -A aleo
+./apoolminer --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_7cqdjrvl2n -A aleo
